b552e25549cdf90165e69b7a95691717f22532f6
Lennaert Meijvogel <l.meijvogel@ogd.nl>
2011-12-03 11:31:26 +0100

    Extract test class Field_ColorAtTest.

---
+++ Tests/Field_ColorAtTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TddTetris;
+using Microsoft.Xna.Framework;
+using Moq;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Field_ColorAtTest
+    {
+        [Test]
+        public void Test_ColorAt_OutOfBounds_RaisesException()
+        {
+            Field subject = new Field(2, 2);
+
+            try
+            {
+                subject.ColorAt(new Vector2(2, 0));
+                Assert.Fail();
+            }
+            catch (Exception) { }
+        }
+
+        [Test]
+        public void Test_ColorAt_WhenBlockIsNull_ReturnsNull()
+        {
+            Field subject = new Field(10, 10);
+            Assert.IsNull(subject.ColorAt(new Vector2(3, 3)));
+        }
+
+        [Test]
+        public void Test_ColorAt_WhenNoBlockIsThere_ReturnsNull()
+        {
+            Field subject = new Field(10, 10);
+            subject.SetBlock(new BlockHelper.MockBlock(), new Vector2(3, 4));
+
+            Assert.IsNull(subject.ColorAt(new Vector2(3, 3)));
+        }
+
+        [Test]
+        public void Test_ColorAt_WhenBlockIsThere_ReturnsBlockColorAt()
+        {
+            Field subject = new Field(10, 10);
+
+            Mock<IBlock> block = new Mock<IBlock>();
+            block.Setup(b => b.ColorAt(new Vector2(1, 2))).Returns(Color.White);
+            block.Setup(b => b.Width).Returns(2);
+            block.Setup(b => b.Height).Returns(3);
+            subject.SetBlock(block.Object, new Vector2(4, 4));
+
+            Color? result = subject.ColorAt(new Vector2(5, 6));
+            Assert.AreEqual(Color.White, result.Value);
+        }
+    }
+}
